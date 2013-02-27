e67f9d798334d265d86a30e0cea914a895999a52
Tanuj <tanuj211@yahoo.com>
2013-02-27 12:07:57 +0000

    Cloned repo from github. Now adding relevant files

---
+++ source/group.cc
@@ -0,0 +1,39 @@
+#include "raycast.h"
+
+// Constructor for a group of n objects.
+Group::Group(int n) {
+
+  this->_object = new Object3D*[n];
+  this->_count  = n;
+  for(int i = 0; i < n; ++i) {
+    this->_object[i] = NULL;
+  }
+}
+
+// Destructor.
+Group::~Group() {
+
+  if (this->_object == NULL) {
+    return;
+  }
+
+  for (int i = 0; i < this->_count; i++) {
+    if (this->_object[i] != NULL) {
+      delete this->_object[i];
+    }
+  }
+  delete[] this->_object;
+}
+
+// Insert an object into the array.
+void Group::addObject(int index, Object3D *obj) {
+
+  // YOUR CODE HERE.
+}
+
+bool Group::intersect(const Ray &r, Hit &h)
+{
+
+  // YOUR CODE HERE.
+
+}
